58cf54a90a838d02a88fba6d2e7c9f020a18ae55
Ruhul Muazzam Rafat <rmrafat420@gmail.com>
2022-11-10 22:57:04 +0600

    assigmnent nearly completed

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 require('dotenv').config()
 const app = express();
@@ -34,12 +34,14 @@ try{
         res.send(services)
     })
 
-    app.get('/services/:id',async(req,res)=>{
-        const query = {};
-        const cursor = serviceCollection.find(query)
-        const services = await cursor.toArray()
+    app.get('/allservices/:id',async(req,res)=>{
+        const id = req.params.id;
+        const query = {_id:ObjectId(id)};
+        const services = await serviceCollection.findOne(query)
         res.send(services)
     })
+
+
 
 }
 finally{
